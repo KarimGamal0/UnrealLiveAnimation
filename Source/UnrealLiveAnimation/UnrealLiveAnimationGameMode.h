e9691f700722f4f727057afc0e3c438ef5587e00
Karim_Gamal0 <karim.gamal.cs@gmail.com>
2022-06-23 14:38:55 +0200

    Add config and Source Folder

---
+++ Source/UnrealLiveAnimation/UnrealLiveAnimationGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealLiveAnimationGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUnrealLiveAnimationGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUnrealLiveAnimationGameMode();
+};
+
+
+
